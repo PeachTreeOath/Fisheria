9483c6462399a9747d5c7973d1a11c102b025869
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-27 01:09:01 -0700

    Lib and scene foundation

---
+++ Assets/PeachLib/AudioManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Audio manager that loads in all sounds from the Audio folder. Use the file names as arguments to play.
+/// Requires an AudioChannel prefab with an audio source component in it.
+/// </summary>
+public class AudioManager : Singleton<AudioManager>
+{
+
+    private AudioSource musicChannel;
+    private AudioSource soundChannel;
+    private Dictionary<string, AudioClip> soundMap;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        soundMap = new Dictionary<string, AudioClip>();
+
+        musicChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        musicChannel.transform.SetParent(transform);
+        musicChannel.loop = true;
+        soundChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        soundChannel.transform.SetParent(transform);
+
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Audio");
+        foreach (AudioClip clip in clips)
+        {
+            soundMap.Add(clip.name, clip);
+        }
+
+        //Kick off initial theme here
+        //PlayMusicWithIntro("exampleIntro", "exampleLoop", .25f);
+    }
+
+    public void PlayMusic(string name, float volume)
+    {
+        musicChannel.clip = soundMap[name];
+        musicChannel.volume = volume;
+        musicChannel.Play();
+    }
+
+    public void PlayMusicWithIntro(string introName, string loopName, float volume)
+    {
+        PlayMusic(introName, volume);
+        StartCoroutine(PlayMusicDelayed(loopName, volume, musicChannel.clip.length));
+    }
+
+    private IEnumerator PlayMusicDelayed(string name, float volume, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        PlayMusic(name, volume);
+    }
+
+    public void PlaySound(string name)
+    {
+        AudioClip clip = soundMap[name];
+        soundChannel.PlayOneShot(soundMap[name]);
+    }
+
+    public void PlaySound(string name, float volume)
+    {
+        soundChannel.PlayOneShot(soundMap[name], volume);
+    }
+
+    public void ToggleMute(bool mute)
+    {
+        if (mute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
+
+}
---
+++ Assets/Resources/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : Singleton<SceneTransitionManager>
+{
+    public void StartGame()
+    {
+        SceneManager.UnloadSceneAsync("MainMenu");
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
+    }
+
+    public void GoToShop()
+    {
+        SceneManager.UnloadSceneAsync("Game");
+        SceneManager.LoadScene("Shop", LoadSceneMode.Additive);
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.UnloadSceneAsync("Shop");
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
+    }
+    
+}
