0361faa063e37eabb2abd8b03cdcb1078ae3701a
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-29 18:12:52 -0500

    Mp controls

---
+++ Assets/Resources/Scripts/FishermanController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishermanController : MonoBehaviour
+{
+
+    public float speed;
+    public int playerNum;
+    private bool isCasting;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal" + playerNum) < 0)
+        {
+            float newX = transform.position.x - (speed * Time.deltaTime);
+            transform.position = new Vector2(newX, transform.position.y);
+        }
+        else if (Input.GetAxis("Horizontal" + playerNum) > 0)
+        {
+            float newX = transform.position.x + (speed * Time.deltaTime);
+            transform.position = new Vector2(newX, transform.position.y);
+        }
+    }
+}
