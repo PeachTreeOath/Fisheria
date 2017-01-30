42fb390afb238e57b776367447a8aae2c2e23ebb
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-29 23:26:11 -0500

    Basic cast

--- Assets/Resources/Scripts/FishermanController.cs
+++ Assets/Resources/Scripts/FishermanController.cs
@@ -5,30 +5,72 @@ using UnityEngine;
 public class FishermanController : MonoBehaviour
 {
 
-    public float speed;
-    public int playerNum;
-    private bool isCasting;
-    
+    // Stats
+    public int speedLevel;
+    public int rangeLevel;
+    public int rodLevel;
 
+    // State
+    public int playerNum;
+    public float speedBase;
+    public float speedMult;
+    private CastState castState;
+    private HookController hook;
+
+    public delegate void CastDelegate();
+    protected CastDelegate castCallback;
 
     // Use this for initialization
     void Start()
     {
-
+        hook = GetComponentInChildren<HookController>();
+        castCallback = CastFinished;
+        hook.CastHook(castCallback, speedLevel, rangeLevel, rodLevel);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal" + playerNum) < 0)
+        if (castState == CastState.READY)
         {
-            float newX = transform.position.x - (speed * Time.deltaTime);
-            transform.position = new Vector2(newX, transform.position.y);
+            if (Input.GetAxis("Horizontal" + playerNum) < 0)
+            {
+                float newX = transform.position.x - ((speedBase + (speedLevel * speedMult)) * Time.deltaTime);
+                transform.position = new Vector2(newX, transform.position.y);
+            }
+            else if (Input.GetAxis("Horizontal" + playerNum) > 0)
+            {
+                float newX = transform.position.x + ((speedBase + (speedLevel * speedMult)) * Time.deltaTime);
+                transform.position = new Vector2(newX, transform.position.y);
+            }
+
+            if (Input.GetButtonDown("FireA" + playerNum))
+            {
+                CastRod();
+            }
         }
-        else if (Input.GetAxis("Horizontal" + playerNum) > 0)
+        else if(castState == CastState.CASTING)
         {
-            float newX = transform.position.x + (speed * Time.deltaTime);
-            transform.position = new Vector2(newX, transform.position.y);
+            if (Input.GetAxis("Horizontal" + playerNum) < 0)
+            {
+                hook.Move(-1);
+            }
+            else if (Input.GetAxis("Horizontal" + playerNum) > 0)
+            {
+                hook.Move(1);
+            }
         }
     }
+
+    private void CastRod()
+    {
+        castState = CastState.CASTING;
+    }
+
+    // Callback when hook returns back to player
+    public void CastFinished()
+    {
+        Debug.Log("TEST");
+    }
+
 }
