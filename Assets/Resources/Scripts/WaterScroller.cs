a771f46ef8139f8b938503ebe8cb063e5047dbae
PeachTreeOath <peachtreeoath@gmail.com>
2017-02-02 00:12:44 -0700

    Water anim

---
+++ Assets/Resources/Scripts/WaterScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroller : MonoBehaviour
+{
+
+    public float scrollSpeed;
+
+    private MeshRenderer meshRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, 0);
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+    }
+}
