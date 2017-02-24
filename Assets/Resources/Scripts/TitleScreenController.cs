71606f6fbc2df156e1ddb46004f05a303aaaf1de
PeachTreeOath <peachtreeoath@gmail.com>
2017-02-23 23:57:12 -0700

    Title screen music

--- Assets/Resources/Scripts/TitleScreenController.cs
+++ Assets/Resources/Scripts/TitleScreenController.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 public class TitleScreenController : MonoBehaviour
 {
 
+    void Start()
+    {
+        AudioManager.instance.PlayMusic("prophet", 1);
+    }
+
     // Update is called once per frame
     void Update()
     {
         for (int i = 1; i < 5; i++)
         {
-            if(Input.GetButtonDown("FireA" + i) || Input.GetButtonDown("FireB" + i))
+            if (Input.GetButtonDown("FireA" + i) || Input.GetButtonDown("FireB" + i))
             {
                 SceneTransitionManager.instance.StartGame();
             }
